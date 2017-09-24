bdca3cbc513caffa2c417f040d081cc2a3b884bd
Donato Perconti <donato.perconti@gmail.com>
2017-09-23 22:09:19 -0700

    Basic updates and settings

--- tm_api/urls.py
+++ tm_api/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from rest_auth.tests.urls import FacebookLogin, TwitterLogin
 
 from tm_api.main import example_endpoint
 
 urlpatterns = [
-    url(r'^api/v1/hello/$', example_endpoint, name='sign S3'),
+    url(r'^rest-auth/facebook/$', FacebookLogin.as_view(), name='fb_login'),
+    url(r'^rest-auth/twitter/$', TwitterLogin.as_view(), name='twitter_login')
 ]
