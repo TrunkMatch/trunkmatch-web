37bb5cd92a3ac9022db79cb749e70e6e8ebed978
Donato Perconti <donato.perconti@gmail.com>
2017-09-27 16:08:41 -0700

    enable local development while still using zappa settings

--- manage.py
+++ manage.py
@@ -7,4 +7,13 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
+    if 'SERVERTYPE' not in os.environ:
+        import json
+        import os
+
+        json_data = open('zappa_settings.json')
+        env_vars = json.load(json_data)['dev']['environment_variables']
+        for key, val in env_vars.items():
+            os.environ[key] = val
+
     execute_from_command_line(sys.argv)
